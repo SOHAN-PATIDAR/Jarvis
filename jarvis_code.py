06690b76e1fd0e25422a2289e638569606caa441
Sohan Patidar <patelsohan1005@gmail.com>
2022-11-15 16:24:31 +0530

    voice_to_text

--- jarvis_code.py
+++ jarvis_code.py
@@ -1,14 +1,25 @@
 import pyttsx3
-
+import speech_recognition as sr
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
 
 
-def speak(st):
+def tell(st):
     '''This function is used to speak some text given given as string'''
     engine.say(st)
     engine.runAndWait()
     
-    
-speak("Hello How are you")
+def speak():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        tell("Speak your msg")
+        msg = r.listen(source)
+        msg_text = r.recognize_google(msg)
+        
+    return(msg_text)
+
+
+
+tell("Hello How are you")
+print(speak())
