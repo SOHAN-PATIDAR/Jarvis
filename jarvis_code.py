f81ca1c267f9a5e2ad3958330a296c0189831891
Sohan Patidar <patelsohan1005@gmail.com>
2022-11-16 19:59:37 +0530

    greetings

--- jarvis_code.py
+++ jarvis_code.py
@@ -1,25 +1,43 @@
 import pyttsx3
 import speech_recognition as sr
+import datetime
+
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
 
 
-def tell(st):
-    '''This function is used to speak some text given given as string'''
+def speak_msg(st):
+    '''This function is used to convert given text string into voice form'''
     engine.say(st)
     engine.runAndWait()
     
-def speak():
+def tell_msg():
+    '''This function is used to convert voice message into text format'''
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        tell("Speak your msg")
+        speak_msg("Speak your msg")
         msg = r.listen(source)
         msg_text = r.recognize_google(msg)
         
     return(msg_text)
 
 
+def wish_me():
+    '''This function is used to wish Good Morning, Good Afternoon and Good Evening as per time'''
+    hour = datetime.datetime.now().hour
+    
+    if hour>=0 and hour<=12:
+        speak_msg("Good Morning Sir, how may i help you")
+    elif hour>12 and hour<=18:
+        speak_msg("Good Afternoon Sir, how may i help you")
+    else:
+        speak_msg("Good Evening Sir, how may i help you")
 
-tell("Hello How are you")
-print(speak())
+
+
+
+if __name__ =="__main__":
+    wish_me()
+    speak_msg("Hello How are you")
+    print(tell_msg())
