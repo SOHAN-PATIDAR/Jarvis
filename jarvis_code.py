45433af896ac9aedb97d506192374672ccfda9a7
Sohan Patidar <patelsohan1005@gmail.com>
2022-11-16 20:18:30 +0530

    perform task

--- jarvis_code.py
+++ jarvis_code.py
@@ -1,6 +1,10 @@
 import pyttsx3
 import speech_recognition as sr
 import datetime
+from time import sleep
+import os
+import webbrowser , wikipedia
+
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -23,6 +27,14 @@ def tell_msg():
     return(msg_text)
 
 
+def set_values(r):
+    '''Setting specific values for different parameters (optional)'''
+    r.pause_threshold = 0.5
+    r.energy_threshold = 300
+    r.non_speaking_duration = 0.2
+    r.phrase_threshold = 0
+
+
 def wish_me():
     '''This function is used to wish Good Morning, Good Afternoon and Good Evening as per time'''
     hour = datetime.datetime.now().hour
@@ -35,9 +47,62 @@ def wish_me():
         speak_msg("Good Evening Sir, how may i help you")
 
 
+def task(query):
+    
+    if "wikipedia" in query:
+        speak_msg("Searching Wikipedia....")
+        query.replace("wikipedia", "")
+        result = wikipedia.summary(query , sentences=2)
+        print(result)
+        speak_msg(f"Here is what I found..., {result}")
+
+    elif "open google" in query:
+        webbrowser.open("google.com")
+
+    elif "open youtube" in query:
+        webbrowser.open("youtube.com")  
+
+    elif "open code" in query:
+        os.startfile("C:\\Users\\ASUS\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe")
+    
+    elif "time"in query:
+        str_time = datetime.datetime.now().strftime("%H:%M:%S")
+        speak_msg(str_time)
+
+    elif "quit" in query:
+        speak_msg("Jarvis is pleased to help you sir")
+        exit()
+
+
+def input_query():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Jarvis is listening sir,")
+        speak_msg("Jarvis is listening sir")
+        
+        set_values(r)
+        
+        print("listening")
+        audio = r.listen(source)
+        query = ''
+        print("listened")
+    try:
+        print('Recognising the command')
+        speak_msg('Recognising the command')
+        query = r.recognize_google(audio)
+        print(f"User said: {query}")
+
+    except Exception as e:
+        print("Can't Recognise the command, Please say that again")
+        speak_msg("Can't Recognise the command, Please say that again")
+        
+    return query
 
 
 if __name__ =="__main__":
     wish_me()
-    speak_msg("Hello How are you")
-    print(tell_msg())
+    while True:
+        command = input_query().lower()
+        task(command)
+        print("\n")
+        sleep(3)
